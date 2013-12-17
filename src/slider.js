57579b5f40017d2a01df30dce39f3eeaa9a4a1a2
dirkgroenen <dirk_groenen@live.nl>
2013-12-17 10:34:41 +0100

    Restructure folders

--- src/slider.js
+++ src/slider.js
@@ -1,3 +1,21 @@
+/*
+	Simple jQuery Slider is just what is says it is: a simple but powerfull jQuery slider.
+	Copyright (C) 2013 - Dirk Groenen [Bitlabs Development]
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 function simpleSlider(useroptions){
 	var obj = this,sliderInterval;
 	obj.currentSlide = 0;
